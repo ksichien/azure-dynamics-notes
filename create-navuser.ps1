5cd3781d7ac3a3fa7087f5bbac3d5dcd810aacb1
K Sichien <ksichien@posteo.net>
2017-11-02 12:48:25 +0100

    changed a cmdlet name

--- create-navuser.ps1
+++ create-navuser.ps1
@@ -1,9 +1,9 @@
 import-module 'C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1'
 $navinst = 'DynamicsNAV101'
 $domain = 'vandelayindustries.com'
-$users = get-childitem "C:\users.txt"
+$users = get-content "C:\users.txt"
 $permissions = 'SUPER'
 foreach ($user in $users) {
     New-NAVServerUser -ServerInstance $navinst -WindowsAccount "$user@$domain" -LicenseType Full -State Enabled
-    New-NAVServerUserPermissionSet -PermissionSetId $permissions -ServerInstance $navsrv -WindowsAccount "$user@$domain"
+    New-NAVServerUserPermissionSet -PermissionSetId $permissions -ServerInstance $navinst -WindowsAccount "$user@$domain"
 }
