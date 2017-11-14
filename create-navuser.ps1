a9b9f8f458382491a34eba4c888273631739382d
K Sichien <ksichien@posteo.net>
2017-11-14 16:28:05 +0100

    refactoring

--- create-navuser.ps1
+++ create-navuser.ps1
@@ -1,7 +1,7 @@
 import-module 'C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1'
 $navinst = 'DynamicsNAV101'
 $domain = 'vandelayindustries.com'
-$users = get-content "C:\users.txt"
+$users = @('art','ssadadmin')
 $permissions = 'SUPER'
 foreach ($user in $users) {
     New-NAVServerUser -ServerInstance $navinst -WindowsAccount "$user@$domain" -LicenseType Full -State Enabled
