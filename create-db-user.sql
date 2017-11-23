a5f45644685706118f296a5d1ad70fc08a7f06b6
K Sichien <ksichien@posteo.net>
2017-11-23 12:08:47 +0100

    revised permissions

--- create-db-user.sql
+++ create-db-user.sql
@@ -3,4 +3,7 @@ create login [avandelay] with password = 'P@ssword!'
 create user [avandelay] from login [avandelay];
 -- in db
 create user [avandelay] from login [avandelay];
-exec sp_addRoleMember 'db_owner', 'avandelay';
+alter role db_owner add member [avandelay]; -- debugging
+alter role db_ddladmin add member [avandelay]; -- alter create drop
+alter role db_datawriter add member [avandelay]; -- insert update delete
+alter role db_datareader add member [avandelay]; -- select
