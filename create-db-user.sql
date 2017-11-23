a72e8f7d8d7a46de2812a2d861d5e0b6129b9a8c
K Sichien <ksichien@posteo.net>
2017-11-23 13:30:23 +0100

    revised permissions

--- create-db-user.sql
+++ create-db-user.sql
@@ -1,9 +1,9 @@
 -- in master
 create login [avandelay] with password = 'P@ssword!'
-create user [avandelay] from login [avandelay];
 -- in db
 create user [avandelay] from login [avandelay];
-alter role db_owner add member [avandelay]; -- debugging
+alter role db_owner add member [avandelay]; -- debugging with breakpoints
 alter role db_ddladmin add member [avandelay]; -- alter create drop
 alter role db_datawriter add member [avandelay]; -- insert update delete
 alter role db_datareader add member [avandelay]; -- select
+grant showplan to [avandelay]; -- required if not a member of db_owner
