7b50392aedd5c6dfb12afd5e8d996fa5a08b5082
K Sichien <ksichien@posteo.net>
2018-01-08 13:13:11 +0100

    updated add user

--- create-nav-user.ps1
+++ create-nav-user.ps1
@@ -1,10 +1,13 @@
 #!/usr/bin/env pwsh
 import-module 'C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1'
-$navinst = 'DynamicsNAV100'
+$navinstances = @('DynamicsNAV100')
 $domain = 'vandelayindustries.com'
 $users = @('art','ssadadmin')
 $permissions = 'SUPER'
+
 foreach ($user in $users) {
-    New-NAVServerUser -ServerInstance $navinst -WindowsAccount "$user@$domain" -LicenseType Full -State Enabled
-    New-NAVServerUserPermissionSet -PermissionSetId $permissions -ServerInstance $navinst -WindowsAccount "$user@$domain"
+    foreach ($navinst in $navinstances) {
+        New-NAVServerUser -ServerInstance $navinst -UserName $user -AuthenticationEmail "$user@$domain" -LicenseType Full -State Enabled
+        New-NAVServerUserPermissionSet -PermissionSetId $permissions -ServerInstance $navinst -UserName $user
+    }
 }
