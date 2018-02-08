339c3b18e3c1322fb3f9f84b58c95ec88dc8d7a1
ksichien <ksichien@posteo.net>
2018-02-08 11:31:26 +0100

    added warning variable

--- monitoring/check-active-connections.ps1
+++ monitoring/check-active-connections.ps1
@@ -1,15 +1,16 @@
 #!/usr/bin/env pwsh
 Import-Module 'C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1'
 
-$limit = 50
+$limit = 50 # sessions allowed by the nav license
+$warning = 0.8 # percentage at which a warning will be issued
 $navinstances = @('nav100-live','nav100-dev')
 
 foreach ($navinstance in $navinstances) {
     $count = (get-NAVServerSession -ServerInstance $navinstance).count
     write-output "current nav instance: ${navinstance}"
     if ($count -ne 0) {
+        $maxpct = [int]($limit*$warning)
         $currentpct = [int](($count/$limit)*100)
-        $maxpct = [int]($limit*0.8)
         write-output "total number of user sessions allowed by the nav license: ${limit}"
         write-output "maximum number of user sessions allowed before a warning is issued: ${maxpct}"
         write-output "current number of user sessions: ${count}"
